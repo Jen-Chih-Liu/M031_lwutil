46946ed4d65489163910ecfce787c851dcfc7808
MB20 JCLiu <JCLiu@nuvoton.com>
2022-05-10 15:38:21 +0800

    first commit

---
+++ SampleCode/StdDriver/lwutil/main.c
@@ -0,0 +1,87 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @version  V3.00
+ * @brief    Show how to set GPIO pin mode and use pin data input/output control.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * @copyright (C) 2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include "stdio.h"
+#include "NuMicro.h"
+#include "lwutil.h"
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable HIRC */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+
+    /* Waiting for HIRC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    /* Switch HCLK clock source to HIRC */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+
+    /* Set both PCLK0 and PCLK1 as HCLK/2 */
+    CLK->PCLKDIV = (CLK_PCLKDIV_APB0DIV_DIV2 | CLK_PCLKDIV_APB1DIV_DIV2);
+
+    /* Switch UART0 clock source to HIRC */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+
+    /* Enable UART peripheral clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate PllClock, SystemCoreClock and CycylesPerUs automatically. */
+    SystemCoreClockUpdate();
+
+    /*----------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                              */
+    /*----------------------------------------------------------------------*/
+    /* Set GPB multi-function pins for UART0 RXD and TXD */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk)) |
+                    (SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+/*----------------------------------------------------------------------*/
+/* Init UART0                                                           */
+/*----------------------------------------------------------------------*/
+void UART0_Init(void)
+{
+    /* Reset UART0 */
+    SYS_ResetModule(UART0_RST);
+
+    /* Configure UART0 and set UART0 baud rate */
+    UART_Open(UART0, 115200);
+}
+
+int32_t main(void)
+{
+    uint8_t arr[10];
+
+    /* Init System, IP clock and multi-function I/O. */
+    SYS_Init();
+
+    /* Init UART0 for printf */
+    UART0_Init();
+
+    printf("\n\nCPU @ %dHz\n", SystemCoreClock);
+
+    lwutil_st_u32_le(0x1234, arr);
+
+    uint32_t val = 0x01;
+    val = lwutil_bits_set(val, 0x12340);
+
+    printf("0x%x\r\n", (unsigned)val);
+
+    lwutil_u32_to_8asciis(0x12345678, (char *)arr);
+    printf("ARR: %s\r\n", arr);
+
+
+    while (1);
+}
